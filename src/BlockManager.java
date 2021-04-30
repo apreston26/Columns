743044917bd25454c03d24fca484c5a5a9c811c1
apreston26 <austinpreston314@gmail.com>
2021-04-30 15:09:22 -0600

    Added basic symbol generator

--- src/BlockManager.java
+++ src/BlockManager.java
@@ -1,2 +1,72 @@
+import java.util.Arrays;
+import java.util.Random;
+
 public class BlockManager {
+
+    int numOfRows;
+    int numOfCols;
+    Random random = new Random();
+    int symbolNumber = random.nextInt(4);
+    int testNum;
+    int counter = 0;
+
+    BlockManager(int rows, int cols) {
+        numOfRows = rows;
+        numOfCols = cols;
+        testNum = random.nextInt(rows);
+
+    }
+
+    private String[][] boardCreator() throws InterruptedException {
+        String[][] board = new String[numOfRows][numOfCols];
+        for (int i = 0; i < numOfRows; i++) {
+            for (int j = 0; j < numOfCols; j++) {
+                board[i][j] = ".";
+
+
+
+                // Symbol Generator
+                if (symbolNumber == 0) {
+                    board[testNum][0 + j] = "*";
+                    Thread.sleep(50);
+                } else if (symbolNumber == 1) {
+                    board[testNum][0 + j] = "%";
+                    Thread.sleep(50);
+                } else if (symbolNumber == 2) {
+                    board[testNum][0 + j] = "/";
+                    Thread.sleep(50);
+                } else if (symbolNumber == 3) {
+                    board[testNum][0 + j] = "$";
+                    Thread.sleep(50);
+                } else {
+                    board[testNum][0 + j] = "?";
+                    Thread.sleep(50);
+                }
+                if (!board[i][j].equals(".")) {
+                    counter++;
+                }
+
+            }
+            System.out.println();
+        }
+        return board;
+    }
+
+
+    @Override
+    public String toString() {
+        String trimmedString = null;
+        try {
+            trimmedString = Arrays.deepToString(boardCreator());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        trimmedString = trimmedString
+                .replace("[[", " ")
+                .replace(",", "")
+                .replace("[", "")
+                .replace("]", "\n");
+        return trimmedString;
+
+    }
 }
--- src/BlockManagerTest.java
+++ src/BlockManagerTest.java
@@ -1,2 +1,23 @@
 public class BlockManagerTest {
+
+
+    public static void main(String[] args) {
+        BlockManagerTest test = new BlockManagerTest();
+        test.bigBoard();
+        test.smallBoard();
+    }
+
+    private void smallBoard(){
+        BlockManager smallBoard = new BlockManager(3,3);
+        String board = smallBoard.toString();
+        System.out.print(board);
+        System.out.println("Objects on Screen: " + smallBoard.counter );
+    }
+
+    private void bigBoard() {
+        BlockManager bigBoard = new BlockManager(7,7);
+        String board = bigBoard.toString();
+        System.out.print(board);
+        System.out.println("Objects on Screen: " + bigBoard.counter );
+    }
 }
