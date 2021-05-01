204bcb85cf28fb6a503b94e6bc69823e9154f6ae
apreston26 <austinpreston314@gmail.com>
2021-05-01 15:47:36 -0600

    Added a way to drop pieces down (roughly)

--- src/BlockManager.java
+++ src/BlockManager.java
@@ -8,10 +8,10 @@ public class BlockManager {
     int numOfBlocks;
     int symbolNumber;
     int counter = 0;
-    int pieceSize = 3;
     Random random = new Random();
     String boardString;
     String newPiece = "";
+    String[][] board;
 
     BlockManager(int rows, int cols, int blockTypes) {
         numOfRows = rows;
@@ -20,41 +20,73 @@ public class BlockManager {
         symbolNumber = random.nextInt(4);
     }
 
-    private String[][] boardCreator() {
-        String[][] board = new String[numOfRows][numOfCols];
+    public String[][] boardCreator() {
+        board = new String[numOfRows][numOfCols];
         for (int i = 0; i < numOfRows; i++) {
             for (int j = 0; j < numOfCols; j++) {
                 board[i][j] = ".";
-                if (!board[i][j].equals(".")) {
-                    counter++;
-                }
             }
         }
         return board;
     }
 
     public String pieceGenerator() {
-        for (int i = 0; i < pieceSize; i++) {
-            int firstPiece = random.nextInt(pieceSize);
-            switch (firstPiece) {
-                case 0 -> newPiece += "a";
-                case 1 -> newPiece += "b";
-                case 2 -> newPiece += "c";
-
-            }
+        int firstPiece = random.nextInt(numOfBlocks);
+        switch (firstPiece) {
+            case 0 -> newPiece = "a";
+            case 1 -> newPiece = "b";
+            case 2 -> newPiece = "c";
+            case 3 -> newPiece = "d";
+            case 4 -> newPiece = "e";
+            case 5 -> newPiece = "f";
         }
         return newPiece;
     }
 
-    @Override
-    public String toString() {
-        boardString = Arrays.deepToString(boardCreator());
+
+    private String[][] boardUpdater(String[][] boardToUse, String pieceA,
+                                    String pieceB, String pieceC) {
+        int piecePlacement = random.nextInt(numOfCols);
+
+            if (boardToUse[numOfRows - 1][piecePlacement].equals(".") &&
+                    (boardToUse[numOfRows - 2][piecePlacement].equals(".")) &&
+                    (boardToUse[numOfRows - 3][piecePlacement].equals("."))) {
+                boardToUse[numOfRows - 1][piecePlacement] = pieceA;
+                boardToUse[numOfRows - 2][piecePlacement] = pieceB;
+                boardToUse[numOfRows - 3][piecePlacement] = pieceC;
+
+            } else if (boardToUse[numOfRows - 4][piecePlacement].equals(".") &&
+                (boardToUse[numOfRows - 5][piecePlacement].equals(".")) &&
+                (boardToUse[numOfRows - 6][piecePlacement].equals("."))) {
+                boardToUse[numOfRows - 4][piecePlacement] = pieceA;
+                boardToUse[numOfRows - 5][piecePlacement] = pieceB;
+                boardToUse[numOfRows - 6][piecePlacement] = pieceC;
+            }
+        return board;
+    }
+
+    public String blankBoard() {
+        board = boardCreator();
+        boardString = Arrays.deepToString(board);
         boardString = boardString
                 .replace("[[", " ")
                 .replace(",", "")
                 .replace("[", "")
                 .replace("]", "\n");
         return boardString;
-
     }
+
+    public String printNewBoard() {
+        board = boardUpdater(board, pieceGenerator(),
+                pieceGenerator(), pieceGenerator());
+        boardString = Arrays.deepToString(board);
+        boardString = boardString
+                .replace("[[", " ")
+                .replace(",", "")
+                .replace("[", "")
+                .replace("]", "\n");
+        return boardString;
+    }
+
+
 }
--- src/BlockManagerTest.java
+++ src/BlockManagerTest.java
@@ -1,24 +1,39 @@
+import java.util.Random;
+
 public class BlockManagerTest {
 
+    Random random = new Random();
+    int minSize = 5;
+    int maxSize = 10;
+    int rowSize = (int) (Math.random() * (maxSize - minSize)) + minSize;
+    int colSize = (int) (Math.random() * (maxSize - minSize)) + minSize;
+    int numOfBlockTypes = 3;
+    int numOfTurns = 10;
 
     public static void main(String[] args) {
         BlockManagerTest test = new BlockManagerTest();
-        test.smallBoard();
+        test.bigBoard();
+
     }
 
     private void smallBoard(){
-        BlockManager smallBoard = new BlockManager(3,3, 4);
+        BlockManager smallBoard = new BlockManager(10, 10, numOfBlockTypes);
         String board = smallBoard.toString();
+        System.out.println("Test first block manager");
         System.out.print(board);
         System.out.println("Objects on Screen: " + smallBoard.counter );
-        System.out.println("piece " + smallBoard.pieceGenerator() );
+        System.out.println(smallBoard.pieceGenerator() );
     }
 
     private void bigBoard() {
-        BlockManager bigBoard = new BlockManager(7,7, 4);
-        String board = bigBoard.toString();
+        BlockManager bigBoard = new BlockManager(6, 4, numOfBlockTypes);
         System.out.println("Test second block manager");
-        System.out.print(board);
-        System.out.println("Objects on Screen: " + bigBoard.counter );
+        System.out.println("Created empty game with " + rowSize + " rows, " +
+                colSize + " cols, " + numOfBlockTypes + " block types");
+        System.out.println(bigBoard.blankBoard());
+        for (int i = 0; i < numOfTurns - 1; i++) {
+
+            System.out.println(bigBoard.printNewBoard());
+        }
     }
 }
